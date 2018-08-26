95588295957564b24a1d0ebbedaf19fad32c9322
Aleksandar <39736298+Tula99@users.noreply.github.com>
2018-08-26 19:20:27 +0200

    Create A_Plus_B.cpp

---
+++ A_Plus_B.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    
+    cout<<a+b<<endl;
+    return 0;
+}
